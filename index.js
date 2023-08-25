5027d3a5d352469b8e8ca9a1c89402b92bddb5a1
M. Sai Vasanth <92867026+msaivasanth@users.noreply.github.com>
2023-08-25 10:56:39 +0530

    Update index.js added path

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const connectToMongo = require('./db')
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
 dotenv.config();
