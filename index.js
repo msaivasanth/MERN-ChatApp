a9999329c9a6758988920f43dc1f966365106fc6
M. Sai Vasanth <92867026+msaivasanth@users.noreply.github.com>
2023-08-25 10:25:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,24 @@ app.use('/api/user', require('./routes/user'))
 app.use('/api/chat', require('./routes/chats'))
 app.use('/api/message', require('./routes/messages'))
 
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
+
 const server = app.listen(port, ()=> {
     console.log(`app listining to port ${port}`)
 })
