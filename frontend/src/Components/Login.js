d02f2c5b3e32e451d3349b86171da1f26850909b
M. Sai Vasanth <92867026+msaivasanth@users.noreply.github.com>
2023-08-25 11:48:30 +0530

    Update Login.js added render url

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -24,7 +24,7 @@ const Login = () => {
           })
           return ;
         }
-        const response = await fetch("http://localhost:5000/api/user/login", {
+        const response = await fetch("https://talk-a-tive-je3r.onrender.com/api/user/login", {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
